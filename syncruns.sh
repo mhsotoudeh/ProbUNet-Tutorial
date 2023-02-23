f1e9c02b9ef3fbd1568ec0a7108e245577ac2645
Hadi Sotoudeh <mhsotoudeh@yahoo.com>
2023-02-23 08:20:05 -0500

    no syncruns

--- syncruns.sh
+++
@@ -1,3 +0,0 @@
-rsync -a --progress --exclude "model*" --exclude "loss*" -e "ssh -i ~/.ssh/computecanada"                                                                             \
-"sotoudeh@beluga.computecanada.ca:/home/sotoudeh/projects/rrg-lplevass/sotoudeh/ProbUNet-Tutorial/runs/"    \
-"/mnt/d/Academia/Learning/Mini Projects/2023 Probabilistic U-Net Tutorial/ProbUNet-Tutorial/runs/"
